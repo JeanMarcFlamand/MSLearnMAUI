e01f29c405bc3910d72f569a4b176e6e77b94758
Jean-Marc Flamand <7jtf@videotron.ca>
2025-04-14 12:09:28 -0400

    Add Shell Navigation app to  Learn Maui Navigation

---
+++ ShellNavigationApp/ShellNavigation/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace ShellNavigation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("AboutPage", typeof(AboutPage));
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ ShellNavigationApp/ShellNavigation/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ShellNavigation
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGoToAboutPageClicked(object sender, EventArgs e)
+        {
+           Shell.Current.GoToAsync("AboutPage");
+        }
+    }
+
+}
